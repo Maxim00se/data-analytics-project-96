b728b2c4a4794bcb8c9c2cc2fc25f43fd09a3d1d
Maksim Yazikov <yazikovmaksim@icloud.com>
2024-12-22 13:10:05 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,32 +1,37 @@
 with tab as (
-select
-row_number() over(partition by s.visitor_id order by visit_date desc) as rnk,
-s.visitor_id,
-s.visit_date,
-s.source as utm_source,
-s.medium as utm_medium,
-s.campaign as utm_campaign
-from sessions as s
-where s.medium <> 'organic'
+    select
+        s.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        row_number()
+            over (
+                partition by s.visitor_id
+                order by s.visit_date desc
+            )
+        as rnk
+    from sessions as s
+    where s.medium <> 'organic'
 )
 select
-tab.visitor_id,
-tab.visit_date,
-tab.utm_source,
-tab.utm_medium,
-tab.utm_campaign,
-l.lead_id,
-l.created_at,
-l.amount,
-l.closing_reason,
-l.status_id
+    tab.visitor_id,
+    tab.visit_date,
+    tab.utm_source,
+    tab.utm_medium,
+    tab.utm_campaign,
+    l.lead_id,
+    l.created_at,
+    l.amount,
+    l.closing_reason,
+    l.status_id
 from tab
 left join leads as l on tab.visitor_id = l.visitor_id
 where tab.rnk = 1
 order by
-amount desc nulls last,
-visit_date asc,
-utm_source asc,
-utm_medium asc,
-utm_campaign asc
+    amount desc nulls last,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc
 limit 10
