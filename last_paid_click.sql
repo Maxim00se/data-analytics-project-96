eb40762383cfd0886d20cacfb6681f66e2eca82b
Maksim Yazikov <yazikovmaksim@icloud.com>
2024-12-22 11:39:19 +0300

    Add file last_paid_click

---
+++ last_paid_click.sql
@@ -0,0 +1,24 @@
+/*
+ * Создаем витрину Last Paid Click
+ */
+select
+    s.visitor_id,
+    s.visit_date,
+    s.source as utm_source,
+    s.medium as utm_medium,
+    s.campaign as utm_campaign,
+    l.lead_id,
+    l.created_at,
+    l.amount,
+    l.closing_reason,
+    l.status_id
+from sessions as s
+left join leads as l on s.visitor_id = l.visitor_id
+where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+order by
+    amount desc nulls last,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc
+limit 10
